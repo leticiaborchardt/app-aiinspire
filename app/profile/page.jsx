a237899e29740ef90ef74c77a0a475d81aa99d93
Letícia <leticiaborchardt123@gmail.com>
2023-06-01 13:51:49 -0300

    deleting post

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,7 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import Profile from '@components/Profile';
+import Swal from 'sweetalert2';
 
 const MyProfile = () => {
 	const { data: session } = useSession();
@@ -29,7 +30,39 @@ const MyProfile = () => {
 	}
 
 	const handleDelete = async (post) => {
+		Swal.fire({
+			text: 'Are you sure you want to delete this prompt?',
+			showCancelButton: true,
+			confirmButtonText: 'Yes, delete it!',
+			denyButtonText: `Cancel`,
+		}).then((result) => {
+			if (result.isConfirmed) {
+				try {
+					fetch(`/api/prompt/${post._id.toString()}`, {
+						method: 'DELETE'
+					});
 
+					const filteredPosts = posts.filter((p) => p._id !== post._id)
+					setPosts(filteredPosts);
+
+					Swal.fire({
+						toast: true,
+						position: 'top-end',
+						text: 'Post deleted with success!',
+						icon: 'success',
+						showConfirmButton: false,
+						timer: 1700
+					});
+				} catch (error) {
+					Swal.fire({
+						text: 'Something went wrong while processing your request.',
+						icon: 'error'
+					}).then(() => {
+						location.reload();
+					});
+				}
+			}
+		})
 	}
 
 	return (
